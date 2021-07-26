66e57ccc37cd3ea0f67762bb32981e30effca735
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-26 17:48:41 +0200

    alphabetspam

---
+++ src/main/java/com/github/pareronia/kattis/alphabetspam/AlphabetSpam.java
@@ -0,0 +1,211 @@
+package com.github.pareronia.kattis.alphabetspam;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Alphabet Spam
+ * @see <a href="https://open.kattis.com/problems/alphabetspam">https://open.kattis.com/problems/alphabetspam</a>
+ */
+public class AlphabetSpam {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public AlphabetSpam(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private static final int WHITESPACE = 1;
+    private static final int LOWERCASE = 2;
+    private static final int UPPERCASE = 3;
+    private static final int OTHER = 4;
+    private static final List<Integer> GROUPS
+            = List.of(WHITESPACE, LOWERCASE, UPPERCASE, OTHER);
+    
+    private int group(final Character ch) {
+        if (ch == '_') {
+            return WHITESPACE;
+        } else if (Character.isLowerCase(ch)) {
+            return LOWERCASE;
+        } else if (Character.isUpperCase(ch)) {
+            return UPPERCASE;
+        } else {
+            return OTHER;
+        }
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final FastScanner sc) {
+        final String s = sc.next();
+        final Map<Integer, Long> groups
+                = Stream.iterate(0, j -> j < s.length(), j -> j + 1)
+                    .map(s::charAt)
+                    .collect(groupingBy(this::group, HashMap::new, counting()));
+        final List<String> ans = GROUPS.stream()
+                .map(g -> (double) groups.getOrDefault(g, 0L) / s.length())
+                .map(d -> String.format("%.15f", d))
+                .collect(toList());
+        return new Result<>(i, ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (this.sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results
+                    = Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(i, sc))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = AlphabetSpam.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new AlphabetSpam(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(AlphabetSpam.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+}
