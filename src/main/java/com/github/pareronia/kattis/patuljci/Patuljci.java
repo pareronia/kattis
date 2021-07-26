50641be2fb8905a864c4102b41938c894f9c02d9
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-26 17:47:50 +0200

    patuljci

---
+++ src/main/java/com/github/pareronia/kattis/patuljci/Patuljci.java
@@ -0,0 +1,218 @@
+package com.github.pareronia.kattis.patuljci;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+/**
+ * Patuljci
+ * @see <a href="https://open.kattis.com/problems/patuljci">https://open.kattis.com/problems/patuljci</a>
+ */
+public class Patuljci {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Patuljci(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    private Pair<Integer, Integer> findSummants(final int[] a, final int target) {
+		final Set<Integer> seen = new HashSet<>();
+		for (final int n1 : a) {
+			seen.add(n1);
+			final Integer n2 = target - n1;
+			if (n1 != n2 && seen.contains(n2)) {
+			    return Pair.of(n1,  n2);
+			}
+		}
+		throw new IllegalStateException("Unsolvable");
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final FastScanner sc) {
+        final int[] a = new int[9];
+        int sum = 0;
+        for (int j = 0; j < 9; j++) {
+            a[j] = sc.nextInt();
+            sum += a[j];
+        }
+        final Pair<Integer, Integer> unwanted = findSummants(a, sum - 100);
+        final List<Integer> ans = Arrays.stream(a)
+                .filter(j -> j != unwanted.getOne() && j != unwanted.getTwo())
+                .boxed()
+                .collect(toList());
+        return new Result<>(i, ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (this.sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results
+                    = Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(i, sc))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Patuljci.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Patuljci(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Patuljci.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+
+    private static final class Pair<L, R> {
+        private final L one;
+        private final R two;
+
+        private Pair(final L one, final R two) {
+            this.one = one;
+            this.two = two;
+        }
+
+        public static <L, R> Pair<L, R> of(final L one, final R two) {
+            return new Pair<>(one, two);
+        }
+
+        public L getOne() {
+            return one;
+        }
+        
+        public R getTwo() {
+            return two;
+        }
+    }
+}
