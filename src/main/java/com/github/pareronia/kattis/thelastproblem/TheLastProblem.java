30fcf9934cb73e18b51c0894f840534fb6f6507e
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-02 22:03:47 +0200

    thelastproblem

---
+++ src/main/java/com/github/pareronia/kattis/thelastproblem/TheLastProblem.java
@@ -0,0 +1,126 @@
+package com.github.pareronia.kattis.thelastproblem;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Last Problem
+ * @see <a href="https://open.kattis.com/problems/thelastproblem">https://open.kattis.com/problems/thelastproblem</a>
+ */
+public class TheLastProblem {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public TheLastProblem(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final String s) {
+        final String ans = String.format("Thank you, %s, and farewell!", s);
+        return new Result<>(i, List.of(ans));
+    }
+    
+    public void solve() throws IOException {
+        try (final BufferedReader sc = new BufferedReader(new InputStreamReader(this.in))) {
+            final List<Result<?>> results = new ArrayList<>();
+            int i = 1;
+            while (true) {
+                final String s = sc.readLine();
+                if (s == null) {
+                    break;
+                }
+                results.add(handleTestCase(i++, s));
+            }
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = TheLastProblem.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new TheLastProblem(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(TheLastProblem.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+}
