58f78638d77af498eef05843767b15e305cd7ba5
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-07 22:42:54 +0200

    kattissquest

---
+++ src/main/java/com/github/pareronia/kattis/kattissquest/KattissQuest.java
@@ -0,0 +1,260 @@
+package com.github.pareronia.kattis.kattissquest;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Kattis's Quest
+ * @see <a href="https://open.kattis.com/problems/kattissquest">https://open.kattis.com/problems/kattissquest</a>
+ */
+public class KattissQuest {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public KattissQuest(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final PriorityQueue<Pair<Integer, Integer>> q = new PriorityQueue<>(
+                (p1, p2) -> {
+                    final int byEnergy = p1.getOne().compareTo(p2.getOne());
+                    if (byEnergy == 0) {
+                        return -1 * p1.getTwo().compareTo(p2.getTwo());
+                    }
+                    return -1 * byEnergy;
+                }
+        );
+        final List<Integer> anss = new ArrayList<>();
+        for (int j = 0; j < n; j++) {
+            final String c = sc.next();
+            if ("add".equals(c)) {
+                final int e = sc.nextInt();
+                final int g = sc.nextInt();
+                q.add(Pair.of(e, g));
+            } else {
+                int x = sc.nextInt();
+                int ans = 0;
+                Pair<Integer, Integer> qq = null;
+                final List<Pair<Integer, Integer>> tmp = new ArrayList<>();
+                do {
+                    for (int k = 0; k < q.size(); k++) {
+                        qq = q.poll();
+                        if (qq.getOne() > x) {
+                            tmp.add(qq);
+                            qq = null;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (qq != null) {
+                        x -= qq.getOne();
+                        ans += qq.getTwo();
+                    }
+                } while (!q.isEmpty() && x > 0);
+                tmp.forEach(q::add);
+                anss.add(ans);
+            }
+        }
+        return new Result<>(i, anss);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (this.sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results
+                    = Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(i, sc))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = KattissQuest.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new KattissQuest(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(KattissQuest.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+
+    private static final class Pair<L, R> {
+        private final L one;
+        private final R two;
+
+        private Pair(final L one, final R two) {
+            this.one = one;
+            this.two = two;
+        }
+
+        public static <L, R> Pair<L, R> of(final L one, final R two) {
+            return new Pair<>(one, two);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder builder = new StringBuilder();
+            builder.append("Pair [one=").append(one).append(", two=").append(two).append("]");
+            return builder.toString();
+        }
+
+        public L getOne() {
+            return one;
+        }
+        
+        public R getTwo() {
+            return two;
+        }
+    }
+}
