7ae5312e426e29e4eb9fc2bb63519697ac9aad25
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-10 21:02:55 +0200

    zyxab

---
+++ src/main/java/com/github/pareronia/kattis/zyxab/Zyxab.java
@@ -0,0 +1,219 @@
+package com.github.pareronia.kattis.zyxab;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Zyxab
+ * @see <a href="https://open.kattis.com/problems/zyxab">https://open.kattis.com/problems/zyxab</a>
+ */
+public class Zyxab {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Zyxab(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private boolean hasNoRepeatedChars(final String s) {
+        return Stream.iterate(0, i -> i < s.length(), i -> i + 1)
+                .map(i -> Character.valueOf(s.charAt(i)))
+                .collect(groupingBy(c -> c, HashMap::new, counting()))
+                .values().stream()
+                .noneMatch(cnt -> cnt > 1);
+    }
+    
+    private Comparator<String> byLengthAndReverseLexicographically() {
+        return (s1, s2) -> {
+            final Integer len1 = Integer.valueOf(s1.length());
+            final Integer len2 = Integer.valueOf(s2.length());
+            if (len1.equals(len2)) {
+                return -1 * s1.compareTo(s2);
+            }
+            return len1.compareTo(len2);
+        };
+    }
+    
+    private Result<?> handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final String ans = Stream.iterate(0, j -> j < n, j -> j + 1)
+                .map(j -> sc.next())
+                .filter(s -> s.length() >= 5)
+                .filter(this::hasNoRepeatedChars)
+                .sorted(byLengthAndReverseLexicographically())
+                .findFirst()
+                .orElse("neibb!");
+        return new Result<>(i, List.of(ans));
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (this.sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results
+                    = Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(i, sc))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> {
+            r.getValues().stream().map(Object::toString).forEach(this.out::println);
+        });
+    }
+    
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Zyxab.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Zyxab(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Zyxab.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+    
+    private static final class Result<T> {
+        @SuppressWarnings("unused")
+        private final int number;
+        private final List<T> values;
+        
+        public Result(final int number, final List<T> values) {
+            this.number = number;
+            this.values = values;
+        }
+
+        public List<T> getValues() {
+            return values;
+        }
+    }
+}
