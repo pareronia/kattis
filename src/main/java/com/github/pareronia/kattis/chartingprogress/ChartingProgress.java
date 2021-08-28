3307940dca1a2ce934822a3911e2bc71242c755b
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-28 18:19:58 +0200

    chartingprogress - smaller

--- src/main/java/com/github/pareronia/kattis/chartingprogress/ChartingProgress.java
+++ src/main/java/com/github/pareronia/kattis/chartingprogress/ChartingProgress.java
@@ -1,7 +1,6 @@
 package com.github.pareronia.kattis.chartingprogress;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -16,7 +15,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Charting Progress
@@ -24,56 +22,51 @@ import java.util.stream.Stream;
  */
 public class ChartingProgress {
 
-    private final boolean sample;
     private final InputStream in;
     private final PrintStream out;
     
     public ChartingProgress(
             final Boolean sample, final InputStream in, final PrintStream out) {
-        this.sample = sample;
         this.in = in;
         this.out = out;
     }
     
-    private Result<?> handleTestCase(final List<String> input) {
-        final List<Long> l = input.stream()
-                .map(this::countStars)
-                .collect(toList());
+    private void handleTestCase(final List<String> input) {
+        final int[] l = new int[input.size()];
+        for (int j = 0; j < input.size(); j++) {
+            l[j] = countStars(input.get(j));
+        }
         final int len = input.get(0).length();
         int sum = 0;
-        final List<String> plot = new ArrayList<>();
-        for (int j = 0; j < l.size(); j++) {
-            final int stars = l.get(j).intValue();
-            final StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < l.length; j++) {
+            final int stars = l[j];
+            final char[] s = new char[len];
             for (int k = 0; k < sum; k++) {
-                sb.append('.');
+                s[k] = '.';
             }
-            for (int k = 0; k < stars; k++) {
-                sb.append('*');
+            for (int k = sum; k < sum + stars; k++) {
+                s[k] = '*';
             }
             for (int k = sum + stars; k < len; k++) {
-                sb.append('.');
+                s[k] = '.';
             }
-            plot.add(sb.toString());
             sum += stars;
+            for (int k = len - 1; k >= 0; k--) {
+                this.out.print(s[k]);
+            }
+            this.out.println();
         }
-        final List<String> ans = plot.stream()
-                .map(this::reverse)
-                .collect(toList());
-        return new Result<>(ans);
+        this.out.println();
     }
 
-    private Long countStars(final String s) {
-        return Stream.iterate(0, j -> j < s.length(), j -> j + 1)
-                .filter(j -> s.charAt(j) == '*')
-                .count();
-    }
-
-    private String reverse(final String s) {
-        final StringBuilder sb = new StringBuilder();
-        Stream.iterate(s.length() - 1, j -> j >= 0, j -> j - 1)
-                .forEach(j -> sb.append(s.charAt(j)));
-        return sb.toString();
+    private int countStars(final String s) {
+        int ans = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '*') {
+                ans++;
+            }
+        }
+        return ans;
     }
     
     public void solve() throws IOException {
@@ -86,10 +79,8 @@ public class ChartingProgress {
                 }
                 inputs.add(line);
             }
-            final List<Result<?>> results = toBlocks(inputs).stream()
-                    .map(block -> handleTestCase(block))
-                    .collect(toList());
-            output(results);
+            toBlocks(inputs).stream()
+                    .forEach(block -> handleTestCase(block));
         }
     }
     
@@ -114,13 +105,6 @@ public class ChartingProgress {
         return blocks;
     }
 
-    private void output(final List<Result<?>> results) {
-        results.forEach(r -> {
-            r.getValues().stream().map(Object::toString).forEach(this.out::println);
-            this.out.println();
-        });
-    }
-    
     public static void main(final String[] args) throws IOException, URISyntaxException {
         final boolean sample = isSample();
         final InputStream is;
@@ -171,18 +155,6 @@ public class ChartingProgress {
             return "sample".equals(System.getProperty("kattis"));
         } catch (final SecurityException e) {
             return false;
-        }
-    }
-    
-    private static final class Result<T> {
-        private final List<T> values;
-        
-        public Result(final List<T> values) {
-            this.values = values;
-        }
-
-        public List<T> getValues() {
-            return values;
         }
     }
 }
