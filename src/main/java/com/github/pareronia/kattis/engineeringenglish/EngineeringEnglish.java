c3081ff8adaea549176495e6d8aa1f1bbb1c56e9
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-28 18:35:15 +0200

    engineeringenglish

---
+++ src/main/java/com/github/pareronia/kattis/engineeringenglish/EngineeringEnglish.java
@@ -0,0 +1,116 @@
+package com.github.pareronia.kattis.engineeringenglish;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+
+/**
+ * Engineering English
+ * @see <a href="https://open.kattis.com/problems/engineeringenglish">https://open.kattis.com/problems/engineeringenglish</a>
+ */
+public class EngineeringEnglish {
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public EngineeringEnglish(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    public void solve() throws IOException {
+        try (final BufferedReader sc = new BufferedReader(new InputStreamReader(this.in))) {
+            final Set<String> seen = new HashSet<>();
+            sc.lines().forEach(line -> {
+                for (final String word : line.split(" ")) {
+                    final String lcword = word.toLowerCase();
+                    if (seen.contains(lcword)) {
+                        this.out.print(".");
+                    } else {
+                        seen.add(lcword);
+                        this.out.print(word);
+                    }
+                    this.out.print(" ");
+                }
+                this.out.println();
+            });
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = EngineeringEnglish.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new EngineeringEnglish(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(EngineeringEnglish.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+}
