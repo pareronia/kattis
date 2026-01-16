e71f485101084dac30f81a00a20e08960a10fd95
pareronia <49491686+pareronia@users.noreply.github.com>
2026-01-16 09:36:35 +0100

    bagdeposit

---
+++ src/main/java/com/github/pareronia/kattis/bagdeposit/BagDeposit.java
@@ -0,0 +1,156 @@
+package com.github.pareronia.kattis.bagdeposit;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Bag Deposit
+ * @see <a href="https://open.kattis.com/problems/bagdeposit">https://open.kattis.com/problems/bagdeposit</a>
+ */
+public class BagDeposit {
+
+    private final InputStream in;
+    private final PrintStream out;
+
+    public BagDeposit(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    private void handleTestCase(final int i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final List<Deque<Integer>> qs = new ArrayList<>();
+        qs.add(new ArrayDeque<>());
+        qs.getLast().addFirst(Integer.MAX_VALUE);
+        for (int j = 0; j < n; j++) {
+            sc.next();
+            final int r = sc.nextInt();
+            boolean ok = false;
+            for (final Deque<Integer> q : qs) {
+               if (q.peekFirst() > r) {
+                   q.addFirst(r);
+                   ok = true;
+                   break;
+               }
+            }
+            if (!ok) {
+                qs.add(new ArrayDeque<>());
+                qs.getLast().addFirst(r);
+            }
+        }
+        this.out.println(qs.size());
+    }
+
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = isSample() ? sc.nextInt() : 1;
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = BagDeposit.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+
+        new BagDeposit(sample, is, out).solve();
+
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(BagDeposit.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
