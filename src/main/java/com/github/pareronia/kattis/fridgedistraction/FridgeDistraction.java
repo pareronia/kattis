ff713b182e384f110efdc3317038d6bab0e0162d
pareronia <49491686+pareronia@users.noreply.github.com>
2026-01-21 10:23:59 +0100

    fridgedistraction

---
+++ src/main/java/com/github/pareronia/kattis/fridgedistraction/FridgeDistraction.java
@@ -0,0 +1,165 @@
+package com.github.pareronia.kattis.fridgedistraction;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Fridge Distraction
+ * @see <a href="https://open.kattis.com/problems/fridgedistraction">https://open.kattis.com/problems/fridgedistraction</a>
+ */
+public class FridgeDistraction {
+
+    private final InputStream in;
+    private final PrintStream out;
+
+    public FridgeDistraction(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    private void handleTestCase(final int i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        int t = sc.nextInt();
+        final Deque<Character> f = new ArrayDeque<>();
+        for (int j = 0; j < n; j++) {
+           f.addLast((char) ('a' + j));
+        }
+        final Deque<Character> q = new ArrayDeque<>();
+        final List<Character> ans = new ArrayList<>();
+        while (t > 0) {
+            int cnt;
+            if (t >= n) {
+                cnt = n;
+                t -= n;
+            } else {
+                cnt = t;
+                t = 0;
+            }
+            for (int j = 0; j < cnt; j++) {
+                q.addFirst(f.pollFirst());
+            }
+            final char ch = q.pollFirst();
+            ans.add(ch);
+            while (!q.isEmpty()) {
+                f.addFirst(q.pollFirst());
+            }
+            f.addFirst(ch);
+        }
+        this.out.println(ans.size());
+        this.out.println(ans.stream().map(String::valueOf).collect(joining(" ")));
+    }
+
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = isSample() ? sc.nextInt() : 1;
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = FridgeDistraction.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+
+        new FridgeDistraction(sample, is, out).solve();
+
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(FridgeDistraction.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("kattis"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
